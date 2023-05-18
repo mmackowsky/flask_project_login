0166f1fcca233089ad7393abb17e782eaf20ec83
mmackowsky <maciostah@gmail.com>
2023-05-18 10:46:50 +0200

    feat(init): updating init file

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.debug = True
 
-    from .main import login_blueprint, password_blueprint, user_blueprint
+    from .main import login_blueprint, password_blueprint, dashboard_blueprint, logout_blueprint
     app.register_blueprint(login_blueprint)
     app.register_blueprint(password_blueprint)
-    app.register_blueprint(user_blueprint)
+    app.register_blueprint(dashboard_blueprint)
+    app.register_blueprint(logout_blueprint)
 
     return app
