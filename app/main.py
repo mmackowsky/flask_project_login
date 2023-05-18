b820800dcf3fd56282e534f5cde2900ec760b509
mmackowsky <maciostah@gmail.com>
2023-05-18 10:44:43 +0200

    feat(main): updating main file

--- app/main.py
+++ app/main.py
@@ -1,22 +1,33 @@
-from flask import render_template, redirect, url_for, Blueprint, request
+from flask import render_template, redirect, url_for, Blueprint, request, session
 
 
 login_blueprint = Blueprint("login", __name__)
 password_blueprint = Blueprint("password", __name__)
-user_blueprint = Blueprint("user", __name__)
+dashboard_blueprint = Blueprint("dashboard", __name__)
+logout_blueprint = Blueprint("logout", __name__)
 
 
 @login_blueprint.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == "POST":
-        nickname = request.form['nickname']
-        password = request.form['password']
-        return redirect(url_for("user.user", nick=nickname, password=password))
-    else:
+        session.update({"nick": request.form['nickname']})
+    elif request.method == "GET" and "nick" not in session:
         return render_template("login.html")
 
+    return redirect(url_for("dashboard.dashboard"))
 
-@user_blueprint.route('/<nick>')
-def user(nick):
-    return f"<h1> Hello, {nick} </h1>"
 
+@logout_blueprint.route('/logout')
+def logout():
+    if "nick" in session:
+        session.pop("nick")
+
+    return redirect(url_for("login.login"))
+
+
+@dashboard_blueprint.route('/dashboard')
+def dashboard():
+    if "nick" in session:
+        return render_template("dashboard.html", nickname=session["nick"])
+
+    return redirect(url_for("login.login"))
