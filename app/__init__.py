ba7f775163d76d0f643d4e2cb1fab8e625e1db2d
mmackowsky <maciostah@gmail.com>
2023-05-18 10:30:46 +0200

    feat(init): add init file

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.debug = True
+
+    from .main import login_blueprint, password_blueprint, user_blueprint
+    app.register_blueprint(login_blueprint)
+    app.register_blueprint(password_blueprint)
+    app.register_blueprint(user_blueprint)
+
+    return app
